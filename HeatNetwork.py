297e9eeca74f0c5f92037c77e377a2aab4f44112
unix96 <65666659+unix96@users.noreply.github.com>
2020-05-23 18:18:24 +0200

    HeatNetwork1.2
    I have added the objective function

--- HeatNetwork.py
+++ HeatNetwork.py
@@ -7,6 +7,8 @@ from itertools import combinations
 import math as mt
 
 if __name__ == "__main__":
+
+
     # Lets read the excel file which contain all the necessary data to start our OR
 
     # Input Data Preparation #
@@ -39,88 +41,97 @@ if __name__ == "__main__":
             ### Data importation ###
 
 
+    ## Date will be imported in a list if they are one dimension or in a dictionary if they are in two dimensions
+    ## with tuples as keys
+
+    v0 = 4  # This the Cord of the heat source
+
+    # The excel file that we want to read
     InputData = "InputDataEnergySmallInstance.xlsx"
+
+    
     card_V = read_excel_data(InputData, "Nodes")
     set_V = [i for i in range(1, card_V[0] + 1)]
-    print('card_V', set_V)
+    #print('card_V', set_V)
 
     # Coords of the different nodes
     NodesCord = read_excel_data(InputData, "NodesCord")
-    print("Cords: ", NodesCord)
-    print(len(NodesCord))
+    #print("Cords: ", NodesCord)
+    #print(len(NodesCord))
+
     # Sommet source
     v_0 = read_excel_data(InputData, "SourceNum")
 
     # Pertes thermiques fixes (thetaijfix)
     thetafix = read_excel_data(InputData, "vfix(thetaijfix)")
-    print("Thermic loss fix:", thetafix)
+    #print("Thermic loss fix:", thetafix)
 
     # Pertes thermiques variables (thetaijvar)
     thetavar = read_excel_data(InputData, "vvar(thetaijvar)")
-    print("Thermic loss", thetavar)
+    #print("Thermic loss", thetavar)
 
     # Fixed Unit Cost (?)
     fixedUnitCost = read_excel_data(InputData, "FixedUnitCost")
-    print("Fixed Unit cost", fixedUnitCost)
+    #print("Fixed Unit cost", fixedUnitCost)
 
     # Heat generation cost of source
     cheat = read_excel_data(InputData, "cheat(ciheat)")
-    print("heat generation:", cheat)
+    #print("heat generation:", cheat)
 
     # Coûts variables dinvestissement
     cvar = read_excel_data(InputData, "cvar(cijvar)")
-    print("Investissement", cvar)
+    #print("Investissement", cvar)
 
     # Couts de maintenance
     cfix = read_excel_data(InputData, "com(cijom)")
-    print("Cout de maintenance", cfix)
+    #print("Cout de maintenance", cfix)
 
     # Revenus
     crev = read_excel_data(InputData, "crev(cijrev)")
-    print("Revenu", crev)
+    #print("Revenu", crev)
 
     # Temps de fonctionnement de la production
     Tflh = read_excel_data(InputData, "Tflh(Tiflh)")
-    print("Temps de fonction:", Tflh)
+    #print("Temps de fonction:", Tflh[0])
 
     # Betta
     betta = read_excel_data(InputData, "Betta")
-    print("betta", betta[0])
+    #print("betta", betta[0])
 
     # Lambda
     Lambda = read_excel_data(InputData, "Lambda")
-    print("Lambda", Lambda[0])
+    #print("Lambda", Lambda[0])
 
     # Alpha
     alpha = read_excel_data(InputData, "Alpha")
-    print("alpha", alpha[0])
+    #print("alpha", alpha[0])
 
     # Edges demand peak
     d = read_excel_data(InputData, "EdgesDemandPeak(dij)")
-    print("d:", d)
+    #print("d:", d)
 
     # Edges demand annual
     D = read_excel_data(InputData, "EdgesDemandAnnual(Dij)")
-    print("D:", D)
+    #print("D:", D)
 
     # Capacité maximale des tuyaux
     Cmax = read_excel_data(InputData, "Cmax(cijmax)")
-    print("Cmax", Cmax)
+    #print("Cmax", Cmax)
 
     # Capacité maximale de la source
     Qmax = read_excel_data(InputData, "SourceMaxCap(Qimax)")
-    print("Qmax:", Qmax)
+    #print("Qmax:", Qmax)
 
     # Pénalités de non satisfaction de la demande
     pumd = read_excel_data(InputData, "pumd(pijumd)")
-    print("pumd", pumd)
+    #print("pumd", pumd)
 
     # longueurs des canaux
     l = {}
     for i in range(1, 9):
         for j in range(1, 9):
             l[(i, j)] = np.sqrt(((NodesCord[j, 1] - NodesCord[i, 1]) ** 2) + ((NodesCord[j, 2] - NodesCord[i, 2]) ** 2))
-    print("lengths", l)
+    #print("lengths", l)
 
     # Here we create the heatProb variable which it will contain all the problem data
     heatProb = LpProblem("Heat_Network_Optimization", LpMinimize)
@@ -132,7 +143,60 @@ if __name__ == "__main__":
     print("Pin_var", Pin_var)
     Pout_var = LpVariable.dicts('Pout', (set_V, set_V), lowBound=0, cat='Continous')
     print("Pout_var", Pout_var)
+
+    # Now i will write the objective function, to minimize the cosst
     # To make it easier to write I will write each constraint in a variable and the functions in the objective function
+    # and seperate the functions in the objections into seperate variables so it can be easier to understand
+
+    # Here are the functions in the objectif functions
+
+    # Heat Generation cost
+    heat_generation_cost = lpSum((Tflh[0] * cheat[3] / betta[0]) * Pin_var[4][i] for i in range(1, 9))
+
+    # Revenue
+    revenue_lis = []
+    for i in range(1, 9):
+        for j in range(1, 9):
+            revenue_lis.append((-Lambda[0]) * x_var[i][j] * cvar[i, j] * D[i, j])
+
+    revenue = lpSum(revenue_lis)
+
+    # Maintenance cost
+    maintenance_cost_lis = []
+    for i in range(1, 9):
+        for j in range(1, 9):
+            maintenance_cost_lis.append(x_var[i][j] * cfix[i, j] * l[i, j])
+    maintenance_cost = lpSum(maintenance_cost_lis)
+
+    # Fixed Investment Cost
+    fixed_investment_cost_lis = []
+    for i in range(1, 9):
+        for j in range(1, 9):
+            fixed_investment_cost_lis.append(alpha[0] * x_var[i][j] * fixedUnitCost[0] * l[i, j])
+    fixed_investement_cost = lpSum(fixed_investment_cost_lis)
+
+    # Variable Investment Cost
+    variable_Inv_cost_lis = []
+    for i in range(1, 9):
+        for j in range(1, 9):
+            variable_Inv_cost_lis.append(alpha[0] * x_var[i][j] * cvar[i, j] * l[i, j] *
+                                         Pin_var[i][j])
+
+    variable_investment_cost = lpSum(variable_Inv_cost_lis)
+
+
+    # Unmet Demand Penalty
+    unmet_demand_penalty_lis = []
+    for i in range(1, 9):
+        for j in range(1, 9):
+            unmet_demand_penalty_lis.append(0.5*(1-x_var[i][j]-x_var[j][i])*D[i, j]*pumd[i, j])
+
+    # After writing the different terms of the objective function, I will add it in the prob data
+
+    heatProb += (heat_generation_cost + revenue + maintenance_cost + fixed_investement_cost + variable_investment_cost
+                    + unmet_demand_penalty_lis)
+
+    ## Here are the constraints of the Heat Network Problem
 
     for i in range(1, 9):
         # Tree structure constraint
@@ -146,16 +210,15 @@ if __name__ == "__main__":
     # The demand satisfaction
     for i in range(1, 9):
         for j in range(1, 9):
-
             heatProb += ((1 - l[i, j]) * thetavar[i, j] * Pin_var[i][j]) - Pout_var[i][j] - (
-                        x_var[i][j] * (betta[0] * Lambda[0] * d[i, j] + l[i, j] * thetafix[i, j]
-                                       ) == 0)
+                    x_var[i][j] * (betta[0] * Lambda[0] * d[i, j] + l[i, j] * thetafix[i, j]
+                                   ) == 0)
 
-    # Flow equilibrium at each vertex, I supposed that the source node is in 1
+    # Flow equilibrium at each vertex,
 
     for i in range(1, 9):
         for j in range(1, 9):
-            if i != j and j != 1:
+            if i != j and j != v0:
                 heatProb += lpSum([Pin_var[i][j]]) == lpSum([Pout_var[i][j]])
             if i == j:
                 continue
@@ -163,19 +226,30 @@ if __name__ == "__main__":
     # Edge capacity constraint
     for i in range(1, 9):
         for j in range(1, 9):
-            heatProb += Pin_var[i][j] <= x_var[i][j]*Cmax[i, j]
+            heatProb += Pin_var[i][j] <= x_var[i][j] * Cmax[i, j]
 
-    # Source Structural constraint, I suppose that the source is in the first node
+    # Source Structural constraint
     for i in range(1, 9):
-        heatProb += lpSum([x_var[i][1]]) == 0
+        heatProb += lpSum([x_var[i][v0]]) == 0
 
-    # Source's heat  generation capacity, i suppose that the source node is in 1
+    # Source's heat  generation capacity
     for i in range(1, 9):
-        heatProb += lpSum([Pin_var[1][i]]) <= Qmax[1]
+        heatProb += lpSum([Pin_var[v0][i]]) <= Qmax[v0]
 
-    # Tour elimination, i suppose that source node is in 1
+    # Tour elimination,
     for i in range(1, 9):
-        if i != 1:
+        if i != v0:
             heatProb += lpSum([x_var[i][j]] for j in range(1, 9)) >= 1
 
-    #
+    # I will write all the problem data in a file
+
+    heatProb.writeLP("HeatProblem.lp")
+
+    # Now that all the date are putted in we will solve this problem
+
+    heatProb.solve()
+
+    # The status of the problem, if the solution is Infeasible, Optimal, Unbounded etc
+
+    print("Status",LpStatus[heatProb.status])
+    print("The solution for the objective function is:", value(heatProb.objective))
